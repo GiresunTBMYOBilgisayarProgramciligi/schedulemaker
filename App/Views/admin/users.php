74baffedac9e23d1bb87fd4c4e2fee81f626cf5f
sametatabasch <sametatabasch@gmail.com>
2023-02-04 19:11:18 +0300

    kullanıcı listesi datatable ile oluşturuldu

--- App/Views/admin/users.php
+++ App/Views/admin/users.php
@@ -26,6 +26,7 @@ $l=new \App\Models\Lecturer();
     <link rel="stylesheet" href="/plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
     <link rel="stylesheet" href="/plugins/summernote/summernote-bs4.min.css">
+    <link rel="stylesheet" href="/assets/AdminLTE/node_modules/datatables.net-dt/css/jquery.dataTables.min.css">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -80,13 +81,13 @@ $l=new \App\Models\Lecturer();
                             <div class="card-header p-0 border-bottom-0">
                                 <ul class="nav nav-tabs" id="custom-tabs-four-tab" role="tablist">
                                     <li class="nav-item">
-                                        <a class="nav-link active" id="custom-tabs-four-home-tab" data-toggle="pill" href="#custom-tabs-four-home" role="tab" aria-controls="custom-tabs-four-home" aria-selected="true">Liste</a>
+                                        <a class="nav-link active" id="user-action-list-tab" data-toggle="pill" href="#custom-tabs-four-home" role="tab" aria-controls="custom-tabs-four-home" aria-selected="true">Liste</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" id="custom-tabs-four-profile-tab" data-toggle="pill" href="#custom-tabs-four-profile" role="tab" aria-controls="custom-tabs-four-profile" aria-selected="false">Ekle</a>
+                                        <a class="nav-link" id="user-action-add-tab" data-toggle="pill" href="#user-action-add-tabContent" role="tab" aria-controls="custom-tabs-four-profile" aria-selected="false">Ekle</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" id="custom-tabs-four-messages-tab" data-toggle="pill" href="#custom-tabs-four-messages" role="tab" aria-controls="custom-tabs-four-messages" aria-selected="false">Düzenle</a>
+                                        <a class="nav-link" id="user-action-edit-tab" data-toggle="pill" href="#user-action-edit-tabContent" role="tab" aria-controls="custom-tabs-four-messages" aria-selected="false">Düzenle</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link" id="custom-tabs-four-settings-tab" data-toggle="pill" href="#custom-tabs-four-settings" role="tab" aria-controls="custom-tabs-four-settings" aria-selected="false">Settings</a>
@@ -94,14 +95,38 @@ $l=new \App\Models\Lecturer();
                                 </ul>
                             </div>
                             <div class="card-body">
-                                <div class="tab-content" id="custom-tabs-four-tabContent">
-                                    <div class="tab-pane fade show active" id="custom-tabs-four-home" role="tabpanel" aria-labelledby="custom-tabs-four-home-tab">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada lacus ullamcorper dui molestie, sit amet congue quam finibus. Etiam ultricies nunc non magna feugiat commodo. Etiam odio magna, mollis auctor felis vitae, ullamcorper ornare ligula. Proin pellentesque tincidunt nisi, vitae ullamcorper felis aliquam id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin id orci eu lectus blandit suscipit. Phasellus porta, ante et varius ornare, sem enim sollicitudin eros, at commodo leo est vitae lacus. Etiam ut porta sem. Proin porttitor porta nisl, id tempor risus rhoncus quis. In in quam a nibh cursus pulvinar non consequat neque. Mauris lacus elit, condimentum ac condimentum at, semper vitae lectus. Cras lacinia erat eget sapien porta consectetur.
+                                <div class="tab-content" id="user-action-list-tabContent">
+                                    <div class="tab-pane fade show active" id="custom-tabs-four-home" role="tabpanel" aria-labelledby="user-action-list-tab">
+                                        <table id="user-list-table" class="table table-bordered table-hover dataTable dtr-inline">
+                                            <thead>
+                                                <tr>
+                                                    <th>İd</th>
+                                                    <th>Kullanıcı Adı</th>
+                                                    <th>e-Posta</th>
+                                                    <th>Adı</th>
+                                                    <th>Soyadı</th>
+                                                    <th>Kayıt Tarihi</th>
+                                                    <th>Son Giriş Tarihi</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            <?php foreach ($user_list as $user):?>
+                                                <tr class="odd">
+                                                    <td><?= $user->id ?></td>
+                                                    <td><?= $user->user_name ?></td>
+                                                    <td><?= $user->mail ?></td>
+                                                    <td><?= $user->name ?></td>
+                                                    <td><?= $user->last_name ?></td>
+                                                    <td><?= $user->register_date ?></td>
+                                                    <td><?= $user->last_login ?></td>
+                                            </tr>
+                                            <?php endforeach; ?></tbody>
+                                        </table>
                                     </div>
                                     <div class="tab-pane fade" id="custom-tabs-four-profile" role="tabpanel" aria-labelledby="custom-tabs-four-profile-tab">
                                         Mauris tincidunt mi at erat gravida, eget tristique urna bibendum. Mauris pharetra purus ut ligula tempor, et vulputate metus facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas sollicitudin, nisi a luctus interdum, nisl ligula placerat mi, quis posuere purus ligula eu lectus. Donec nunc tellus, elementum sit amet ultricies at, posuere nec nunc. Nunc euismod pellentesque diam.
                                     </div>
-                                    <div class="tab-pane fade" id="custom-tabs-four-messages" role="tabpanel" aria-labelledby="custom-tabs-four-messages-tab">
+                                    <div class="tab-pane fade" id="user-action-edit-tabContent" role="tabpanel" aria-labelledby="user-action-edit-tab">
                                         Morbi turpis dolor, vulputate vitae felis non, tincidunt congue mauris. Phasellus volutpat augue id mi placerat mollis. Vivamus faucibus eu massa eget condimentum. Fusce nec hendrerit sem, ac tristique nulla. Integer vestibulum orci odio. Cras nec augue ipsum. Suspendisse ut velit condimentum, mattis urna a, malesuada nunc. Curabitur eleifend facilisis velit finibus tristique. Nam vulputate, eros non luctus efficitur, ipsum odio volutpat massa, sit amet sollicitudin est libero sed ipsum. Nulla lacinia, ex vitae gravida fermentum, lectus ipsum gravida arcu, id fermentum metus arcu vel metus. Curabitur eget sem eu risus tincidunt eleifend ac ornare magna.
                                     </div>
                                     <div class="tab-pane fade" id="custom-tabs-four-settings" role="tabpanel" aria-labelledby="custom-tabs-four-settings-tab">
@@ -141,9 +166,17 @@ $l=new \App\Models\Lecturer();
 <script src="/assets/AdminLTE/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/demo.min.js"></script>
+<script src="/assets/js/gettext.php"></script>
+<script src="/assets/js/ajax.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/pages/dashboard.min.js"></script>
+<script src="/assets/AdminLTE/node_modules/datatables.net/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('.dataTable').DataTable();
+    });
+</script>
 </body>
 </html>
