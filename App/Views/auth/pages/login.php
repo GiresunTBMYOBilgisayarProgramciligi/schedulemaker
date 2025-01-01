a1b4392b1267a2874edd8bec27779f0412e099df
sametatabasch <sametatabasch@gmail.com>
2025-01-02 00:25:51 +0300

    login sayfası düzenlendi

--- App/Views/auth/pages/login.php
+++ App/Views/auth/pages/login.php
@@ -8,9 +8,9 @@
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="/assets/AdminLTE/node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/assets/node_modules/@fortawesome/fontawesome-free/css/all.min.css">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="/assets/AdminLTE/node_modules/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/node_modules/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/css/adminlte.min.css">
 </head>
@@ -64,9 +64,9 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="/assets/AdminLTE/node_modules/jquery/dist/jquery.min.js"></script>
+<script src="/assets/node_modules/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="/assets/AdminLTE/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"></script>
 <script src="/assets/js/gettext.php"></script>
