5428e60cb353db71af8b26102ff954cfc7393a03
sametatabasch <sametatabasch@gmail.com>
2025-01-01 12:44:00 +0300

    jquery ui kaldırıldı

--- App/Views/admin/theme/footer_scripts.php
+++ App/Views/admin/theme/footer_scripts.php
@@ -1,11 +1,6 @@
 <!-- jQuery -->
 <script src="/assets/AdminLTE/node_modules/jquery/dist/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="/assets/AdminLTE/node_modules/jquery-ui-dist/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-    $.widget.bridge('uibutton', $.ui.button)
-</script>
+
 <!-- Bootstrap 4 -->
 <script src="/assets/AdminLTE/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
