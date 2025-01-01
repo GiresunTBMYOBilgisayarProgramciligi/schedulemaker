742a08d22e676260f73f927de08f091f01749ad4
sametatabasch <sametatabasch@gmail.com>
2025-01-02 00:25:41 +0300

    footer script düzenlendi

--- App/Views/admin/theme/footer_scripts.php
+++ App/Views/admin/theme/footer_scripts.php
@@ -1,17 +1,12 @@
 <!-- jQuery -->
-<script src="/assets/AdminLTE/node_modules/jquery/dist/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="/assets/AdminLTE/node_modules/jquery-ui-dist/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-    $.widget.bridge('uibutton', $.ui.button)
-</script>
+<script src="/assets/node_modules/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="/assets/AdminLTE/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"></script>
-<!-- Data table -->
-<script src="/assets/node_modules/datatables.net-dt/js/dataTables.dataTables.min.js"></script>
+
 <script src="/assets/js/gettext.php"></script>
 <script src="/assets/js/ajax.js"></script>
+<!-- Data table -->
+<script src="/assets/node_modules/datatables.net-dt/js/dataTables.dataTables.min.js"></script>
 <script src="/assets/js/custom.js"></script>
