4aa5c5062533b1c249cee427490d2ee258865249
sametatabasch <sametatabasch@gmail.com>
2025-01-02 18:29:11 +0300

    Navbar boşaltıldı sidebar düzenlendi

--- App/Views/admin/theme/navbar.php
+++ App/Views/admin/theme/navbar.php
@@ -1,16 +1,1 @@
-<!-- Navbar -->
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>
-
-        <h2 class="navbar-brand mb-0 h1 ">
-            <i class="nav-icon fas fa-users"></i>
-            <?=$page_title?>
-        </h2>
-    </ul>
-
-</nav>
-<!-- /.navbar -->
+<!-- todo nav bar silindi ama menünün küçültülme işleminin kullanılması güzel olur. onu taşımanın bir yolunu bul -->
--- App/Views/admin/theme/sidebar.php
+++ App/Views/admin/theme/sidebar.php
@@ -4,13 +4,6 @@
  */
 ?><!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="/admin" class="brand-link">
-        <img src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
-             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">TMYO Ders Programı</span>
-    </a>
-
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
