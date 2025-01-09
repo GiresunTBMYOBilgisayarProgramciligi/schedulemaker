b82a8e1db475fa06f3a8b0842e17d6d418c55453
sametatabasch <sametatabasch@gmail.com>
2025-01-09 15:41:15 +0300

    program için veri tabanı yapısı güncellendi

--- setup.sql
+++ setup.sql
@@ -1,15 +1,27 @@
-CREATE DATABASE if not exists schedule CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE if not exists schedule_maker CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 /* .env dosyasında belirttiğiniz kullanıcı adı ve şifreyi ilgili alanlara yazdıktan sonra çalıştırın*/
 CREATE USER if not exists 'kullanici_adi'@'localhost' IDENTIFIED BY 'parola';
-GRANT ALL PRIVILEGES ON schedule.* TO 'kullanici_adi'@'localhost';
+GRANT ALL PRIVILEGES ON schedule_maker.* TO 'kullanici_adi'@'localhost';
 /*Eğer kullanıcı uzak bağlantı yapacaksa: */
-#GRANT ALL PRIVILEGES ON schedule.* TO 'kullanici_adi'@'%';
+#CREATE USER if not exists 'kullanici_adi'@'%' IDENTIFIED BY 'parola';
+#GRANT ALL PRIVILEGES ON schedule_maker.* TO 'kullanici_adi'@'%';
 FLUSH PRIVILEGES;
-use schedule;
+use schedule_maker;
 create table if not exists schedule
 (
     id   int AUTO_INCREMENT,
     data text,
+    type varchar(20),
+    owner_type varchar(20),
+    owner_id int,
+    time varchar(20),
+    season varchar(20),
+    day0 text,
+    day1 text,
+    day2 text,
+    day3 text,
+    day4 text,
+    day5 text,
     primary key (id)
 ) ENGINE = INNODB;
 
@@ -24,13 +36,11 @@ create table if not exists users
     title         varchar(50),
     department_id int,
     program_id    int,
-    schedule_id    int,
     register_date timestamp   default current_timestamp,
     last_login    timestamp,
     approved      BOOLEAN     DEFAULT false, -- Onay alanı
     primary key (id),
-    unique (mail),
-    foreign key (schedule_id) references schedule (id) on delete set null on update cascade
+    unique (mail)
 ) ENGINE = INNODB;
 
 create table if not exists departments
@@ -38,23 +48,20 @@ create table if not exists departments
     id             int AUTO_INCREMENT,
     name           varchar(100),
     chairperson_id int,
-    schedule_id    int,
     primary key (id),
     unique (name),
-    foreign key (chairperson_id) references users (id) on delete set null on update cascade,
-    foreign key (schedule_id) references schedule (id) on delete set null on update cascade
+    foreign key (chairperson_id) references users (id) on delete set null on update cascade
 ) ENGINE = INNODB;
 
 create table if not exists classrooms
 (
     id         int AUTO_INCREMENT,
     name       varchar(20),
+    plan        text, # oturma planı
     class_size int default 0,
     exam_size  int default 0,
-    schedule_id    int,
     primary key (id),
-    unique (name),
-    foreign key (schedule_id) references schedule (id) on delete set null on update cascade
+    unique (name)
 ) ENGINE = INNODB;
 
 create table if not exists programs
@@ -62,11 +69,9 @@ create table if not exists programs
     id            int AUTO_INCREMENT,
     name          varchar(100) not null,
     department_id int,
-    schedule_id    int,
     primary key (id),
     unique (name),
-    foreign key (department_id) references departments (id) on delete set null,
-    foreign key (schedule_id) references schedule (id) on delete set null on update cascade
+    foreign key (department_id) references departments (id) on delete set null
 ) ENGINE = INNODB;
 
 create table if not exists lessons
@@ -99,8 +104,3 @@ ALTER TABLE users
 insert into users(password, mail, name, last_name, title, role, approved)
 values ('$2y$10$OOqHpMPJhvAR2uyoLFCPAuKTgFJDfEB1CtlrpSnxB9SQIYc/bWqYC', 'admin@admin.com', 'Admin', 'Admin', 'Admin',
         'admin', true);
-
-insert into departments (name, chairperson_id)
-values ('Bilgisayar Teknolojileri', 1);
-insert into programs (name, department_id)
-values ('Bilgisayar Programcılığı', 1)
