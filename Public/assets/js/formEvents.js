cfd85daa9e9d04acbbbc70d0854dc652d4a59e70
sametatabasch <sametatabasch@gmail.com>
2025-01-13 22:37:37 +0300

    hata düzeltme

--- Public/assets/js/formEvents.js
+++ Public/assets/js/formEvents.js
@@ -4,29 +4,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     const departmentSelect = document.getElementById("department_id");
     const programSelect = document.getElementById("program_id");
+    if (departmentSelect) {
+        departmentSelect.addEventListener("change", function () {
+            const departmentId = this.value;
 
-    departmentSelect.addEventListener("change", function () {
-        const departmentId = this.value;
+            // AJAX isteği gönder
+            fetch(`/ajax/getProgramsList/${departmentId}`)
+                .then(response => response.json())
+                .then(data => {
+                    // Program select kutusunu temizle
+                    programSelect.innerHTML = "";
 
-        // AJAX isteği gönder
-        fetch(`/ajax/getProgramsList/${departmentId}`)
-            .then(response => response.json())
-            .then(data => {
-                // Program select kutusunu temizle
-                programSelect.innerHTML = "";
-
-                // Gelen programları ekle
-                data.forEach(program => {
-                    const option = document.createElement("option");
-                    option.value = program.id;
-                    option.textContent = program.name;
-                    programSelect.appendChild(option);
+                    // Gelen programları ekle
+                    data.forEach(program => {
+                        const option = document.createElement("option");
+                        option.value = program.id;
+                        option.textContent = program.name;
+                        programSelect.appendChild(option);
+                    });
+                })
+                .catch(error => {
+                    console.error("Programları alırken bir hata oluştu:", error);
                 });
-            })
-            .catch(error => {
-                console.error("Programları alırken bir hata oluştu:", error);
-            });
-    });
+        });
+    }
 
     /**
      * Formlarda name ve last_name
