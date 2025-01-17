219b6984c54ffd95f3ef0a43c40f9dff6a620009
sametatabasch <sametatabasch@gmail.com>
2025-01-17 19:30:39 +0300

    login sayfası adminlte 4 e güncellendi

--- App/Views/auth/pages/login.php
+++ App/Views/auth/pages/login.php
@@ -1,75 +1,139 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+<!--begin::Head-->
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in (v2)</title>
+    <title>TMYO Ders Programı</title>
+    <!--begin::Fonts-->
+    <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css"
+            integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q="
+            crossorigin="anonymous"
+    />
+    <!--end::Fonts-->
+    <!--begin::Third Party Plugin(OverlayScrollbars)-->
+    <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/styles/overlayscrollbars.min.css"
+            integrity="sha256-tZHrRjVqNSRyWg2wbppGnT833E/Ys0DHWGwT04GiqQg="
+            crossorigin="anonymous"
+    />
+    <!--end::Third Party Plugin(OverlayScrollbars)-->
+    <!--begin::Third Party Plugin(Bootstrap Icons)-->
+    <link rel="stylesheet" href="/assets/node_modules/bootstrap-icons/font/bootstrap-icons.min.css"/>
+    <!--end::Third Party Plugin(Bootstrap Icons)-->
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="/assets/node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="/assets/node_modules/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/css/adminlte.min.css">
+    <!--begin::Required Plugin(AdminLTE)-->
+    <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/admin-lte@4.0.0-beta3/dist/css/adminlte.min.css"
+            crossorigin="anonymous"
+    />
+    <!--end::Required Plugin(AdminLTE)-->
+    <link rel="stylesheet" type="text/css" href="/assets/css/adminlte3to4.css" />
 </head>
-<body class="hold-transition login-page">
+<!--end::Head-->
+<!--begin::Body-->
+<body class="login-page bg-body-secondary">
 <div class="login-box">
-    <!-- /.login-logo -->
     <div class="card card-outline card-primary">
-        <div class="card-header text-center">
-            <a href="../../index2.html" class="h1"><b>TMYO</b>Takvim</a>
+        <div class="card-header">
+            <a
+                    href="/admin"
+                    class="link-dark text-center link-offset-2 link-opacity-100 link-opacity-50-hover"
+            >
+                <h1 class="mb-0"><b>TMYO</b>Takvim</h1>
+            </a>
         </div>
-        <div class="card-body">
-
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">Başlamak için giriş yapın</p>
             <form action="/ajax/login" method="post" class="ajaxForm" title="Giriş Yapılıyor">
-                <div class="input-group mb-3">
-                    <input type="email" class="form-control" placeholder="Email" name="mail" required>
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
+                <div class="input-group mb-1">
+                    <div class="form-floating">
+                        <input id="loginEmail" type="email" class="form-control" value="" placeholder="" name="mail" required/>
+                        <label for="loginEmail">Email</label>
                     </div>
+                    <div class="input-group-text"><span class="bi bi-envelope"></span></div>
                 </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" placeholder="Password" name="password" required>
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
+                <div class="input-group mb-1">
+                    <div class="form-floating">
+                        <input id="loginPassword" type="password" class="form-control" placeholder="" name="password" required/>
+                        <label for="loginPassword">Parola</label>
                     </div>
+                    <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
                 </div>
+                <!--begin::Row-->
                 <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
-                            <input type="checkbox" id="remember_me" name="remember_me">
-                            <label for="remember_me">
-                                Beni hatırla
-                            </label>
+                    <div class="col-8 d-inline-flex align-items-center">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" name="remember_me" />
+                            <label class="form-check-label" for="flexCheckDefault"> Beni hatırla </label>
                         </div>
                     </div>
                     <!-- /.col -->
                     <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">Giriş Yap</button>
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary">Giriş Yap</button>
+                        </div>
                     </div>
                     <!-- /.col -->
                 </div>
+                <!--end::Row-->
             </form>
         </div>
-        <!-- /.card-body -->
+        <!-- /.login-card-body -->
     </div>
-    <!-- /.card -->
 </div>
 <!-- /.login-box -->
-
-<!-- jQuery -->
-<script src="/assets/node_modules/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="/assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"></script>
+<!--begin::Third Party Plugin(OverlayScrollbars)-->
+<script
+        src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
+        integrity="sha256-dghWARbRe2eLlIJ56wNB+b760ywulqK3DzZYEpsg2fQ="
+        crossorigin="anonymous"
+></script>
+<!--end::Third Party Plugin(OverlayScrollbars)-->
+<!--begin::Required Plugin(popperjs for Bootstrap 5)-->
+<script src="/assets/node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+<!--end::Required Plugin(popperjs for Bootstrap 5)-->
+<!--begin::Required Plugin(Bootstrap 5)-->
+<script src="/assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<!--end::Required Plugin(Bootstrap 5)-->
+<!--begin::Required Plugin(AdminLTE)-->
+<script
+        src="https://cdn.jsdelivr.net/npm/admin-lte@4.0.0-beta3/dist/js/adminlte.min.js"
+        crossorigin="anonymous"
+></script>
+<!--end::Required Plugin(AdminLTE)-->
+<!--begin::OverlayScrollbars Configure-->
+<script>
+    const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
+    const Default = {
+        scrollbarTheme: 'os-theme-light',
+        scrollbarAutoHide: 'leave',
+        scrollbarClickScroll: true,
+    };
+    document.addEventListener('DOMContentLoaded', function () {
+        const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+        if (sidebarWrapper && typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== 'undefined') {
+            OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+                scrollbars: {
+                    theme: Default.scrollbarTheme,
+                    autoHide: Default.scrollbarAutoHide,
+                    clickScroll: Default.scrollbarClickScroll,
+                },
+            });
+        }
+    });
+</script>
+<!--end::OverlayScrollbars Configure-->
 <script src="/assets/js/gettext.php"></script>
+<script src="/assets/js/myHTMLElements.js"></script>
 <script src="/assets/js/ajax.js"></script>
+<script type="module" src="/assets/js/custom.js"></script>
+
+<!--end::Script-->
 </body>
+<!--end::Body-->
 </html>
