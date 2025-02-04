c8742469a5f6feceaba1b7244edf27329cc31b2e
sametatabasch <sametatabasch@gmail.com>
2025-02-04 21:13:10 +0300

    Revert "Ufak düzeltmeler"
    This reverts commit f28a5e01e5a9e859c6c5a33c2156ce35694ee4e4.

--- App/Controllers/ScheduleController.php
+++ App/Controllers/ScheduleController.php
@@ -189,7 +189,7 @@ class ScheduleController extends Controller
             $out .= '</tbody>
                </table>';
         } catch (Exception $e) {
-            throw new Exception("Program tablosu oluşturulurken bir hata oluştu", 0,$e);
+            throw new Exception($e->getMessage());
         }
 
         return $out;
