d48c32232ea792de1f3e9c61f4f3d5da25d4b8c2
sametatabasch <sametatabasch@gmail.com>
2025-02-15 11:17:34 +0300

    Helpers auto load a eklendi

--- Public/index.php
+++ Public/index.php
@@ -4,7 +4,6 @@ use Dotenv\Dotenv;
 use App\Core\Application;
 $dotenv = Dotenv::createImmutable(__DIR__."/../App");
 $dotenv->load();
-require $_ENV["APP_PATH"] . "/Helpers.php";
 
 ini_set('display_errors', 1);
 error_reporting(E_ERROR);
