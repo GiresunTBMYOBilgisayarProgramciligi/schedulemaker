871251037cf48bb29e1577c41729380a770676d9
sametatabasch <sametatabasch@gmail.com>
2025-02-24 11:41:23 +0300

    Sadece sınıf mevcudundan büyük dersliklerin listelenmesi sağlandı

--- App/Controllers/ScheduleController.php
+++ App/Controllers/ScheduleController.php
@@ -374,6 +374,7 @@ class ScheduleController extends Controller
                 $lesson = (new LessonController())->getLesson($filters['lesson_id']);
                 unset($filters['lesson_id']);// sonraki sorgularda sorun çıkartmaması için lesson id siliniyor.
                 $classroomFilters["type"] = $lesson->classroom_type;
+                $classroomFilters["class_size"]= ['>'=>$lesson->size];
             }
             $times = $this->generateTimesArrayFromText($filters["time"], $filters["hours"]);
             $unavailable_classroom_ids = [];
@@ -384,6 +385,7 @@ class ScheduleController extends Controller
                             "time" => $times,
                             "owner_type" => $filters['owner_type'],
                             "semester" => $filters['semester'],
+                            "academic_year" => $filters['academic_year'],
                         ]
                     );
                     foreach ($classroomSchedules as $classroomSchedule) {
@@ -393,6 +395,10 @@ class ScheduleController extends Controller
                     }
                     $classroomFilters["!id"] = $unavailable_classroom_ids;
                     $available_classrooms = (new ClassroomController())->getListByFilters($classroomFilters);
+                    if (count($available_classrooms) == 0) {
+                        unset($classroomFilters['class_size']);
+                        $available_classrooms = (new ClassroomController())->getListByFilters($classroomFilters);
+                    }
                 } else {
                     Logger::setErrorLog("owner_type classroom değil");
                     throw new Exception("owner_type classroom değil");
