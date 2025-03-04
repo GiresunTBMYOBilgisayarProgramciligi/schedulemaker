ceff6e59a598f0f32560d6ac0534caa320d78c9a
sametatabasch <sametatabasch@gmail.com>
2025-03-04 13:03:48 +0300

    Sayfa başlığı düzenlendi

--- App/Views/admin/theme/head.php
+++ App/Views/admin/theme/head.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * @var \App\Core\AssetManager $assetManager
+ * @var string $page_title
  */
 ?>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TMYO Ders Programı</title>
+    <title><?=$page_title?> - TMYO Ders Programı</title>
 
     <?= $assetManager->renderCss() ?>
 </head>
