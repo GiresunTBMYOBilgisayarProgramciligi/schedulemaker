6f28bad0a314746e72b24c6a8b7b8b17b4010f49
sametatabasch <sametatabasch@gmail.com>
2025-03-05 21:41:35 +0300

    Program tablosunda hoca linki hatası düzeltildi

--- App/Controllers/ScheduleController.php
+++ App/Controllers/ScheduleController.php
@@ -120,7 +120,7 @@ class ScheduleController extends Controller
                                         </a>
                                         ' . $lesson->getFullName() . '
                                     </div>
-                                    <div><a class="link-light link-underline-opacity-0" href="/admin/lesson/' . $lesson->getLecturer()->id . '\">
+                                    <div><a class="link-light link-underline-opacity-0" href="/admin/profile/' . $lesson->getLecturer()->id . '\">
                                         <i class="bi bi-person-square"></i>
                                     </a>
                                     ' . $lecturerName . '</div>
@@ -157,7 +157,7 @@ class ScheduleController extends Controller
                                         
                                     </div>
                                     <div>
-                                    <a class="link-light link-underline-opacity-0" href="/admin/lesson/' . $day->lecturer_id . '\">
+                                    <a class="link-light link-underline-opacity-0" href="/admin/profile/' . $day->lecturer_id . '\">
                                         <i class="bi bi-person-square"></i>
                                     </a>
                                     ' . $lecturerName . '
