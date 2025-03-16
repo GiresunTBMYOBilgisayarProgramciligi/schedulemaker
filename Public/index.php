0b82e441151e8fc69ceba6c1d0b6c7ce0f4ced81
sametatabasch <sametatabasch@gmail.com>
2025-03-16 15:59:29 +0300

    hata raporlama seviyesi değiştirildi

--- Public/index.php
+++ Public/index.php
@@ -9,7 +9,7 @@ $dotenv->load();
 define("DEBUG_MODE", $_ENV['DEBUG']); // Geliştirme ortamında true, canlı ortamda false
 
 ini_set('display_errors', 1);
-error_reporting(E_ERROR);
+error_reporting(E_ALL); //todo error reporting ayarını geliştirme ortamında ve yayın ortamında nasıl düzenleyeceğim? E_all yerine E_Error mu yapmalıyım bunlar neyi ifade ediyor
 setlocale(LC_ALL, 'tr_TR.UTF-8');
 
 // Hata işleyicisini başlat
