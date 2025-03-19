ca0c389ec09993529329646f0230cd4893855e4c
sametatabasch <sametatabasch@gmail.com>
2025-03-19 11:54:20 +0300

    fetchForm fonksiyonu diğer ajax işlemlerinde de kullanılacak

--- Public/assets/js/ajax.js
+++ Public/assets/js/ajax.js
@@ -22,14 +22,6 @@ document.addEventListener("DOMContentLoaded", function () {
         form.addEventListener("submit", handleAjaxDeleteParentLesson);
     })
 
-    /**
-     *
-     * @type {Modal}
-     * @see /assets/js/myHTMLElements.js
-     */
-    const modal = new Modal();
-    const spinner = new Spinner();
-
     function handleAjaxForm(event) {
         event.preventDefault();
         /**
@@ -49,124 +41,27 @@ document.addEventListener("DOMContentLoaded", function () {
             let file = importFileInput.files[0]
             data.append("file", file)
         }
-        modal.prepareModal(form.getAttribute("title"));
-        spinner.showSpinner(modal.body)
-        modal.showModal();
-
-        fetch(form.action, {
-            method: form.method || "POST",
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-            },
-            body: data,
-        })
-            .then(response => response.json())
-            .then((data) => {
-                console.log(data)
-                if (data.errors) {
-                    console.log(data.errors)
-                }
-                if (data.status === "error") {
-
-                    const statusClass = data.status === "error" ? "danger" : data.status;
-                    modal.body.classList.add("text-bg-" + statusClass)
-                    const message = Array.isArray(data.msg)
-                        ? `<ul>${data.msg.map((item) => `<li>${item}</li>`).join("")}</ul>`
-                        : data.msg;
-                    spinner.removeSpinner();
-                    modal.body.innerHTML = message;
-                } else {
-                    modal.body.classList.add("text-bg-" + data.status)
-
-                    const message = Array.isArray(data.msg)
-                        ? `<ul>${data.msg.map((item) => `<li>${item}</li>`).join("")}</ul>`
-                        : data.msg;
-                    spinner.removeSpinner();
-                    modal.body.innerHTML = message;
-                }
-
-                modal.cancelButton.addEventListener("click", () => {
-                    if (data.redirect) {
-                        if (data.redirect === "back") {
-                            window.history.back()
-                        } else
-                            window.location.href = data.redirect;
-                    }
-                    if (!form.classList.contains("updateForm")) {
-                        form.reset();
-                    }
-                });
-
-            })
-            .catch((error) => {
-                modal.title.textContent = "Hata";
-                modal.body.classList.add("text-bg-danger");
-                modal.body.innerHTML = error;
-                console.error(error);
-            });
+        fetchForm(form, data);
     }
-
 
     // Silme işlemi için onay fonksiyonu
     function handleAjaxDelete(event) {
         let lessonRow = event.target.closest('tr');
         event.preventDefault();
         const form = event.target;
-
-        modal.prepareModal(gettext.confirmDelete, gettext.deleteMessage, true)
-        modal.confirmButton.textContent = gettext.delete
-        modal.showModal()
-        modal.confirmButton.addEventListener("click", () => {
-
-            spinner.showSpinner(modal.body)
-            modal.confirmButton.remove();
-            fetch(form.action, {
-                method: form.method || "POST",
-                headers: {
-                    'X-Requested-With': 'XMLHttpRequest',
-                },
-                body: new FormData(form),
+        let confirmDeleteModal = new Modal();
+        confirmDeleteModal.prepareModal(gettext.confirmDelete, gettext.deleteMessage, true)
+        confirmDeleteModal.confirmButton.textContent = gettext.delete
+        confirmDeleteModal.showModal()
+        confirmDeleteModal.confirmButton.addEventListener("click", () => {
+            confirmDeleteModal.closeModal();
+            fetchForm(form, new FormData(form)).then(() => {
+                if (lessonRow) {
+                    lessonRow.remove();
+                } else {
+                    window.history.back()
+                }
             })
-                .then(response => response.json())
-                .then((data) => {
-                    if (data.status === "error") {
-                        const statusClass = data.status === "error" ? "danger" : data.status;
-                        modal.body.classList.add("text-bg-" + statusClass)
-                        const message = Array.isArray(data.msg)
-                            ? `<ul>${data.msg.map((item) => `<li>${item}</li>`).join("")}</ul>`
-                            : data.msg;
-                        spinner.removeSpinner()
-                        modal.body.innerHTML = message;
-                    } else {
-                        modal.body.classList.add("text-bg-" + data.status)
-
-                        const message = Array.isArray(data.msg)
-                            ? `<ul>${data.msg.map((item) => `<li>${item}</li>`).join("")}</ul>`
-                            : data.msg;
-                        spinner.removeSpinner()
-                        modal.body.innerHTML = message;
-                        if (lessonRow) {
-                            lessonRow.remove();
-                        } else {
-                            window.history.back()
-                        }
-                    }
-
-                    if (data.redirect) {
-                        modal.cancelButton.addEventListener("click", () => {
-                            if (data.redirect === "back") {
-                                window.history.back()
-                            } else
-                                window.location.href = data.redirect;
-                        });
-                    }
-                })
-                .catch((error) => {
-                    modal.title.textContent = "Hata";
-                    modal.body.classList.add("text-bg-danger");
-                    modal.body.innerHTML = error;
-                    console.error(error);
-                });
         });
     }
 
@@ -196,14 +91,12 @@ document.addEventListener("DOMContentLoaded", function () {
             new Toast().prepareToast('Hata', "Lütfen sadece bir seçim yapın", "danger");
             return false;
         } else if (parentLessonId != 0) {
-            console.log("Buders başka derse bağlanacak");
             data.append('child_lesson_id', lessonId);
             let conbineModal = new Modal("CombineLessonModal");
             conbineModal.hideModal();
             fetchForm(form, data)
 
         } else if (childLessonId != 0) {
-            console.log("Bu derse bir ders bağlanacak");
             data.append("parent_lesson_id", lessonId)
             let conbineModal = new Modal("CombineLessonModal");
             conbineModal.hideModal();
@@ -216,17 +109,18 @@ document.addEventListener("DOMContentLoaded", function () {
         const form = event.target;
         let data = new FormData(form);
         let deleteParentModal = new Modal()
-        deleteParentModal.prepareModal("Bağlantı Silme Onayı","Bu dersin bağlantısını silmek istediğinizden emin miziniz?",true,true);
-        modal.confirmButton.textContent = gettext.delete
+        deleteParentModal.prepareModal("Bağlantı Silme Onayı", "Bu dersin bağlantısını silmek istediğinizden emin miziniz?", true, true);
+        deleteParentModal.confirmButton.textContent = gettext.delete
         deleteParentModal.showModal();
-        deleteParentModal.confirmButton.addEventListener("click",(event)=>{
+        deleteParentModal.confirmButton.addEventListener("click", (event) => {
             deleteParentModal.hideModal();
-            fetchForm(form,data)
+            fetchForm(form, data)
         })
     }
 
     function fetchForm(form, data) {
         let modal = new Modal();
+        let spinner = new Spinner();
         modal.prepareModal(form.getAttribute("title"));
         spinner.showSpinner(modal.body)
         modal.showModal();
@@ -260,6 +154,10 @@ document.addEventListener("DOMContentLoaded", function () {
                         : data.msg;
                     spinner.removeSpinner();
                     modal.body.innerHTML = message;
+                    // başarılı işlem sonrası update formları dışınsa kalan formlar resetleniyor
+                    if (!form.classList.contains("updateForm")) {
+                        form.reset();
+                    }
                 }
 
                 modal.cancelButton.addEventListener("click", () => {
@@ -272,12 +170,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         else
                             window.location.href = data.redirect;
                     }
-
-                    if (!form.classList.contains("updateForm")) {
-                        form.reset();
-                    }
                 });
-
             })
             .catch((error) => {
                 modal.title.textContent = "Hata";
