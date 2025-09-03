a16cc2a18efb0031769674a35b068841e4b03389
sametatabasch <sametatabasch@gmail.com>
2025-09-03 18:00:40 +0300

    home index daha sade olacak şekilde düzenlendi

--- App/Views/home/pages/index.php
+++ App/Views/home/pages/index.php
@@ -40,8 +40,31 @@ use function App\Helpers\getSettingValue;
                 <div class="col-12">
                     <div class="card card-primary card-outline">
                         <!-- .card-header -->
-                        <div class="card-header">
-                            <h3 class="card-title">Bölüm ve Program Ders Programı</h3>
+                        <div class="card-header d-flex justify-content-between align-items-center">
+                            <h3 class="card-title flex-fill">Bölüm ve Program Ders Programı</h3>
+                            <div class=" flex-fill">
+                                <div class="input-group ">
+                                    <select class="form-select" id="academic_year" name="academic_year">
+                                        <?php for ($year = 2023; $year <= date('Y'); $year++): ?>
+                                            <option value="<?= $year . ' - ' . $year + 1 ?>" <?= getSettingValue("academic_year") == $year . ' - ' . $year + 1 ? 'selected' : '' ?>>
+                                                <?= $year . ' - ' . $year + 1 ?>
+                                            </option>
+                                        <?php endfor; ?>
+                                    </select>
+                                    <span class="input-group-text"> - </span>
+                                    <select class="form-select" id="semester" name="semester">
+                                        <option value="Güz" <?= getSettingValue("semester") == 'Güz' ? 'selected' : '' ?>>
+                                            Güz
+                                        </option>
+                                        <option value="Bahar" <?= getSettingValue("semester") == 'Bahar' ? 'selected' : '' ?>>
+                                            Bahar
+                                        </option>
+                                        <option value="Yaz" <?= getSettingValue("semester") == 'Yaz' ? 'selected' : '' ?>>
+                                            Yaz
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-lte-toggle="card-collapse">
                                     <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
@@ -53,9 +76,8 @@ use function App\Helpers\getSettingValue;
                         <div class="card-body">
                             <!--begin::Row-->
                             <div class="row">
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <label class="form-label" for="department_id">Bölüm</label>
+                                <div class="col-12 mb-3">
+                                    <div class="input-group">
                                         <select class="form-select tom-select" id="department_id" name="department_id">
                                             <?php array_unshift($departments, (object)["id" => 0, "name" => "Bölüm Seçiniz"]);
                                             foreach ($departments as $department): ?>
@@ -64,142 +86,59 @@ use function App\Helpers\getSettingValue;
                                                 </option>
                                             <?php endforeach; ?>
                                         </select>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <label class="form-label" for="program_id">Program</label>
                                         <select class="form-select" id="program_id" name="program_id">
                                             <option value="0">İlk olarak Bölüm seçiniz</option>
                                         </select>
-
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="mb-3">
-                                        <label class="form-label" for="academic_year]">Dönem</label>
-                                        <div class="input-group ">
-                                            <select class="form-select" id="academic_year" name="academic_year">
-                                                <?php for ($year = 2023; $year <= date('Y'); $year++): ?>
-                                                    <option value="<?= $year . ' - ' . $year + 1 ?>" <?= getSettingValue("academic_year") == $year . ' - ' . $year + 1 ? 'selected' : '' ?>>
-                                                        <?= $year . ' - ' . $year + 1 ?>
-                                                    </option>
-                                                <?php endfor; ?>
-                                            </select>
-                                            <span class="input-group-text"> - </span>
-                                            <select class="form-select" id="semester" name="semester">
-                                                <option value="Güz" <?= getSettingValue("semester") == 'Güz' ? 'selected' : '' ?>>
-                                                    Güz
-                                                </option>
-                                                <option value="Bahar" <?= getSettingValue("semester") == 'Bahar' ? 'selected' : '' ?>>
-                                                    Bahar
-                                                </option>
-                                                <option value="Yaz" <?= getSettingValue("semester") == 'Yaz' ? 'selected' : '' ?>>
-                                                    Yaz
-                                                </option>
-                                            </select>
-                                        </div>
+                                        <button type="button" class="btn btn-primary"
+                                                id="departmentAndProgramScheduleButton">
+                                            Göster
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                             <!--end::Row-->
-                        </div>
-                        <!--end::card-body-->
-                        <div class="card-footer card-primary">
                             <div class="row">
-                                <div class="text-end">
-                                    <input class="btn btn-primary" type="button" name="departmentAndProgramScheduleButton" id="departmentAndProgramScheduleButton"
-                                           value="Göster">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--end::Row-->
-            <!--start::Row-->
-            <div class="row  mb-3">
-                <div class="col-6">
-                    <div class="card card-primary card-outline">
-                        <!-- .card-header -->
-                        <div class="card-header">
-                            <h3 class="card-title">Öğreti Üyesi / Görevlisi Ders Programı</h3>
-                            <div class="card-tools">
-                                <button type="button" class="btn btn-tool" data-lte-toggle="card-collapse">
-                                    <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
-                                    <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="mb-3">
-                                        <select class="form-select tom-select" id="lecturer_id" name="lecturer_id" placeholder=" Öğretim Üyesi / Görevlisi Seçimek izin yazınız">
+                                <div class="col-6">
+                                    <div class=" input-group mb-3">
+                                        <select class="form-select tom-select " id="lecturer_id" name="lecturer_id"
+                                                placeholder=" Öğretim Üyesi / Görevlisi Seçimek izin yazınız">
                                             <option></option>
                                             <?php foreach ($lecturers as $lecturer): ?>
                                                 <option value="<?= $lecturer->id ?>"><?= $lecturer->getFullName() ?></option>
                                             <?php endforeach; ?>
                                         </select>
-
+                                        <button class="btn btn-primary" type="button" id="lecturerScheduleButton">
+                                            Göster
+                                        </button>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <!--end::card-body-->
-                        <div class="card-footer card-primary">
-                            <div class="row">
-                                <div class="text-end">
-                                    <input class="btn btn-primary" type="button" name="lecturerScheduleButton" id="lecturerScheduleButton" value="Göster">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="card card-primary card-outline">
-                        <!-- .card-header -->
-                        <div class="card-header">
-                            <h3 class="card-title">Derslik Ders Programı</h3>
-                            <div class="card-tools">
-                                <button type="button" class="btn btn-tool" data-lte-toggle="card-collapse">
-                                    <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
-                                    <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="mb-3">
+                                <div class="col-6">
+                                    <div class="input-group mb-3">
                                         <select class="form-select" id="classroom_id" name="classroom_id">
                                             <option value="0">Derslik Seçiniz</option>
                                             <?php foreach ($classrooms as $classroom): ?>
                                                 <option value="<?= $classroom->id ?>"><?= $classroom->name ?></option>
                                             <?php endforeach; ?>
                                         </select>
-
+                                        <button class="btn btn-primary" type="button" id="classroomScheduleButton">
+                                            Göster
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <!--end::card-body-->
-                        <div class="card-footer card-primary">
-                            <div class="row">
-                                <div class="text-end">
-                                    <input class="btn btn-primary" type="button" name="classroomScheduleButton" id="classroomScheduleButton" value="Göster">
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
             <!--end::Row-->
-            <div id="schedule_container">
-                <!-- Programlar buraya yüklenecek -->
+            <!--start::Row-->
+            <div class="row  mb-3">
+                <div id="schedule_container">
+                    <!-- Programlar buraya yüklenecek -->
+                </div>
             </div>
+            <!--end::Row-->
         </div>
         <!--end::Container-->
     </div>
