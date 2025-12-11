ea06ce71da777744cd44134291749a7cc0e790e2
sametatabasch <sametatabasch@gmail.com>
2025-12-11 13:23:32 +0300

    düzeltme

--- App/Controllers/ProgramController.php
+++ App/Controllers/ProgramController.php
@@ -22,9 +22,6 @@ class ProgramController extends Controller
      */
     public function getProgramsList(array $filters = []): array
     {
-        if (!isset($filters["department_id"])) {
-            unset($filters["department_id"]);
-        }
         return $this->getListByFilters($filters);
     }
 
