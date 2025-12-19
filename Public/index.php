b2dfecbeb50eea90d56331e1bfff88e96f82ce08
sametatabasch <sametatabasch@gmail.com>
2025-12-19 13:58:00 +0300

    log saatleri düzenlendi

--- Public/index.php
+++ Public/index.php
@@ -10,6 +10,7 @@ define("DEBUG_MODE", $_ENV['DEBUG']); // Geliştirme ortamında true, canlı ort
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL); //todo error reporting ayarını geliştirme ortamında ve yayın ortamında nasıl düzenleyeceğim? E_all yerine E_Error mu yapmalıyım bunlar neyi ifade ediyor
+date_default_timezone_set('Europe/Istanbul');
 setlocale(LC_ALL, 'tr_TR.UTF-8');
 
 // Hata işleyicisini başlat
