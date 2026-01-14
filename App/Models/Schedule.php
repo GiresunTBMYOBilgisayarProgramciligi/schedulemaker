2fdd3f34e6200459bcf609af7109cf14ce3e52bf
sametatabasch <sametatabasch@gmail.com>
2026-01-14 18:48:21 +0300

    hatalı birleştirme düzeltmesi

--- App/Models/Schedule.php
+++ App/Models/Schedule.php
@@ -35,7 +35,6 @@ class Schedule extends Model
     public ?int $semester_no = null;
     public ?string $semester = null;
     public ?string $academic_year = null;
-    protected array $excludeFromDb = [];
 
     /**
      * @var ScheduleItem[]
