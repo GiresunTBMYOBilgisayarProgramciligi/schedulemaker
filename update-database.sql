710361211ba3db00ba91530a5c3575b7c5b297d7
sametatabasch <sametatabasch@gmail.com>
2026-07-16 15:09:54 +0300

    kısıtlama adı düzenlendi

--- update-database.sql
+++ update-database.sql
@@ -1,5 +1,5 @@
 #v0.2.7 -> v0.2.8
-ALTER TABLE lessons DROP FOREIGN KEY lessons_ibfk_4;
-ALTER TABLE lessons DROP FOREIGN KEY lessons_ibfk_5;
+ALTER TABLE lessons DROP FOREIGN KEY lessons_ibfk_6;
+ALTER TABLE lessons DROP FOREIGN KEY lessons_ibfk_7;
 ALTER TABLE lessons DROP COLUMN exam_parent_lesson_id;
 ALTER TABLE lessons DROP COLUMN parent_lesson_id;
