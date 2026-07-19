ebd18513445efe90bc220fe9c08d19befcb24d34
sametatabasch <sametatabasch@gmail.com>
2026-07-19 18:50:19 +0300

    eksik yetkiler eklendi

--- App/Enums/UserRole.php
+++ App/Enums/UserRole.php
@@ -12,7 +12,9 @@ enum UserRole: string
     case Admin = 'admin';
     case Manager = 'manager';
     case SubManager = 'submanager';
+    case Secretary = 'secretary';
     case DepartmentHead = 'department_head';
+    case ResearchAssistant = 'research_assistant';
     case Lecturer = 'lecturer';
     case User = 'user';
 
@@ -26,7 +28,9 @@ enum UserRole: string
             self::Admin => 'Yönetici',
             self::Manager => 'Müdür',
             self::SubManager => 'Müdür Yardımcısı',
+            self::Secretary => 'Sekreter',
             self::DepartmentHead => 'Bölüm Başkanı',
+            self::ResearchAssistant => 'Araştırma Görevlisi',
             self::Lecturer => 'Akademisyen',
             self::User => 'Kullanıcı',
         };
@@ -38,22 +42,22 @@ enum UserRole: string
      */
     public static function getAssignableRoles(): array
     {
-        $roles = [self::User, self::Lecturer];
+        $roles = [self::User, self::Lecturer, self::ResearchAssistant];
         
         if (Gate::allowsRole("admin")) {
             $roles = array_merge(
                 $roles,
-                [self::DepartmentHead, self::SubManager, self::Manager, self::Admin]
+                [self::DepartmentHead, self::Secretary, self::SubManager, self::Manager, self::Admin]
             );
         } elseif (Gate::allowsRole("manager")) {
             $roles = array_merge(
                 $roles,
-                [self::DepartmentHead, self::SubManager, self::Manager]
+                [self::DepartmentHead, self::Secretary, self::SubManager, self::Manager]
             );
         } elseif (Gate::allowsRole("submanager")) {
             $roles = array_merge(
                 $roles,
-                [self::DepartmentHead]
+                [self::DepartmentHead, self::Secretary]
             );
         }
         
