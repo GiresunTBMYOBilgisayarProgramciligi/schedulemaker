1d37db4ccf8e78a3dc6e57e85422be6cc2e66b76
sametatabasch <sametatabasch@gmail.com>
2026-07-20 15:54:55 +0300

    veri tabanı güncellemesine not eklendi

--- update-database.sql
+++ update-database.sql
@@ -1,6 +1,6 @@
 #v0.2.7 -> v0.2.8
-ALTER TABLE lessons DROP FOREIGN KEY lessons_ibfk_6;
-ALTER TABLE lessons DROP FOREIGN KEY lessons_ibfk_7;
+ALTER TABLE lessons DROP FOREIGN KEY lessons_ibfk_6; # kısıtlama adı düzenlenmeli 
+ALTER TABLE lessons DROP FOREIGN KEY lessons_ibfk_7; # kısıtlama adı düzenlenmeli
 ALTER TABLE lessons DROP COLUMN exam_parent_lesson_id;
 ALTER TABLE lessons DROP COLUMN parent_lesson_id;
 
