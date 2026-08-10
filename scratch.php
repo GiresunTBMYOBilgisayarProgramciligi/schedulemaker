30701b557c6c4ac5e5652d283f37b5c51d0f1423
sametatabasch <sametatabasch@gmail.com>
2026-08-10 22:54:46 +0300

    gereksiz dosyalar silindi

--- scratch.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$pdo = \App\Core\Database::getInstance()->getConnection();
-$stmt = $pdo->query("SELECT setting_value FROM settings WHERE setting_key='user_180'");
-print_r($stmt->fetchColumn());
